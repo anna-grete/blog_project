90451e532a2f27486de102ff3c1b000d607f2ef7
annagrrr <annagrete.arak@gmail.com>
2022-01-25 16:49:40 +0200

    fourth

--- src/routes/user/getUsers.test.ts
+++ src/routes/user/getUsers.test.ts
@@ -1,9 +1,18 @@
+import axios from 'axios';
+
+const endpoint = 'http://localhost:3000';
+
 describe ('users',() => {
     beforeAll(() => {
 //käivitatakse enne testi paki algust (nt. tee test andmebaasi ja täida see)
     });
-    it('should return user by ID',() => {
-
+    //käivita testid
+    it('should return error for non existing ID',() => {
+       const response = await axios.get(endpoint + '/nonExistentID');
+       const data = response.data;
+       expect(data).toHaveProperty('error');
+       expect(data.error).toEqual('No user found with given ID');
+       return;
     });
     afterAll(() => {
 //käivitatakse peale testi pakki (nt. kustuta test andmebaas)
