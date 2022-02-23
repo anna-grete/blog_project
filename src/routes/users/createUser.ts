5fcb60859a695612ddbe53eb7eda564a5d8692da
anna-grete <90323697+anna-grete@users.noreply.github.com>
2022-02-23 09:55:49 +0200

    update

--- src/routes/users/createUser.ts
+++ src/routes/users/createUser.ts
@@ -1,44 +1,65 @@
 import express, { Request, Response } from 'express';
 import User from '../../entities/User';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
+import bunyan from 'bunyan';
+const log = bunyan.createLogger({
+  name: __filename,
+  stream: process.stdout
+});
+
 const router = express.Router();
 
 interface UserInput {
-    firstName: string,
-    middleName?: string,
-    lastName: string,
-    mobile: string,
-    email: string,
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  mobile: string;
+  email: string;
 }
 
 router.post('/', async (req: Request, res: Response) => {
-    try{ 
-        let { 
-            firstName, 
-            middleName, 
-            lastName, 
-            mobile, 
-            email 
-        } = req.body as UserInput;
+  try {
+    let {
+      firstName,
+      middleName,
+      lastName,
+      mobile,
+      email
+    } = req.body as UserInput;
 
- //TODO: validation for inputs
+    // TODO: validation for inputs
 
- const user = new User();
- user.id = uuidv4();
-user.firstName = firstName, 
-user.middleName = middleName =! null ? middleName : '';
-user.lastName = lastName;
-user.mobile = mobile;
-user.email = email;
+    const user = new User();
+    user.id = uuidV4();
+    user.firstName = firstName;
+    user.middleName = middleName = !null ? middleName : '';
+    user.lastName = lastName;
+    user.mobile = mobile;
+    user.email = email;
 
-let newUser = await user.save();
-if(!newUser) {
-    throw new Error();
-}
-res.json(newUser);
-} catch(error){
-    res.json({error: "Unable to create new user", message: 'Cannot read properties from v4'});
-};
+    let newUser = await user.save();
+    if (!newUser) {
+      throw new Error();
+    }
+    log.info({ user: newUser }, 'New user was created');
+
+    return res.json(newUser);
+  } catch (error) {
+    log.error( { error: error, input: req.body }, 'Unable to create User');
+
+    if (error instanceof Error) {
+
+      return res.json({
+        error: 'Unable to create new user',
+        message: error.message
+      });
+    }
+
+    return res.json({
+      error: 'Unable to create new user',
+      message: 'unknown error'
     });
+  }
+});
 
-   export default router;
+export default router;
--- src/routes/users/getUsers.test.ts
+++ src/routes/users/getUsers.test.ts
@@ -11,14 +11,17 @@ describe ('users',() => {
         const response = await axios.get(
             endpoint + '/089ddda5-f4c8-4bca-974a-e69d616e504a'
         );
-        expect(response?.data).toHaveProperty('id');
-        expect(response?.data?.title).toEqual('Title for a random post');
+        const data = response.data;
+        
+        expect(data).toHaveProperty('id');
+    expect(data.id).toEqual('089ddda5-f4c8-4bca-974a-e69d616e504a');
+    return;
     });
     it('should return error for non existing ID', async ()=> {
        const response = await axios.get(endpoint + '/089ddda5-f4c8-4bca-974a-e69d616e504a');
        const data = response.data;
        console.log(data);
-       expect(data).toHaveProperty('id');
+       expect(data).toHaveProperty('message');
        expect(data?.message).toEqual('no user found with given ID');
        return;
     });
