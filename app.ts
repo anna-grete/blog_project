a7850d3e05f0622628033414f96cb1cf7787bee5
anna-grete <90323697+anna-grete@users.noreply.github.com>
2022-02-23 09:44:36 +0200

    update

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import router from './routes/index';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const App = express();
+App.use(express.json());
+App.use(cors());
+
+App.use(helmet({
+  crossOriginResourcePolicy: {
+    policy: 'cross-origin',
+  }
+}));
+
+App.use(router);
+App.get('/', (req, res) => {
+  res.json({ message: 'Hello!' });
+});
+
+export = App;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const http = require('http');
-
-const server = http.createServer((req, res)=>{
-    res.statuscode = 200;
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer((req, res) => {
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello');
 });
-
-server.listen(3000,()=>{
+let message = "Hello";
+server.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
 });
