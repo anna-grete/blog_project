64834a50ed5b5f5e25fa0ca6ad905c68ca3f9be7
anna-grete <90323697+anna-grete@users.noreply.github.com>
2022-02-23 09:53:35 +0200

    Delete src/src directory

--- src/src/routes/users/getUserPosts.ts
+++
@@ -1,7 +0,0 @@
-import express, { Request, Response } from 'express';
-import Post from '../../entities/Post';
-const router = express.Router();
-
-router.get('/:id/posts');
-
-export default router;
